f5ea28a8dbed31d9857123227a6fc86737277aa5
dwerner7 <daiana.werner95@gmail.com>
2023-07-03 18:15:47 -0300

    front

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
+// navbar-light bg-light">
+// <div class="container">
+// <a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+
+// </div>
+// </nav>`
+
+document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
+navbar-light border-radius-10">
+<div class="container">
+<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+
+</div>
+</nav>`
