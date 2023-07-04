c8306ad6a8378244c059300ea79084e9ae7c7c13
dwerner7 <daiana.werner95@gmail.com>
2023-07-04 18:12:00 -0300

    index

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
 navbar-light border-radius-10">
 <div class="container">
-<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+<a class="navbar-brand" href="../templates/index.html">CRUD</a>
 
 </div>
 </nav>`
